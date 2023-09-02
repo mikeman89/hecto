908b1e4ebff8f5e731a54462b19a1df7c89f8f89
Michael Al Tork <michael.altork@gmail.com>
2023-09-02 10:01:52 -0400

    add termion to toml file and compile

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,12 @@
+use std::io::{self, Read};
+
 fn main() {
-    println!("Hello, world!");
+    for b in io::stdin().bytes() {
+        let c = b.unwrap() as char;
+        println!("{}", c);
+        if c == 'q' {
+            break;
+        }
+    }
 }
 // this is a note
