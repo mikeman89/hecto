631d25a50d9367896e839b29de2d1c6307700fa2
Michael Al Tork <michael.altork@gmail.com>
2023-09-04 16:46:11 -0400

    add logic for opening a file from command line

--- src/editor.rs
+++ src/editor.rs
@@ -1,5 +1,6 @@
 use crate::Row;
 use crate::{Document, Terminal};
+use std::env;
 use termion::event::Key;
 
 const VERSION: &str = env!("CARGO_PKG_VERSION");
@@ -32,11 +33,18 @@ impl Editor {
         }
     }
     pub fn default() -> Self {
+        let args: Vec<String> = env::args().collect();
+        let document = if args.len() > 1 {
+            let file_name = &args[1];
+            Document::open(&file_name).unwrap_or_default()
+        } else {
+            Document::default()
+        };
         Self {
             should_quit: false,
             terminal: Terminal::default().expect("Failed to initialize terminal"),
             cursor_position: Position::default(),
-            document: Document::default(),
+            document,
         }
     }
 
