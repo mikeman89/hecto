1024fccd6915c62876e3aa090171de705f950b7c
Michael Al Tork <michael.altork@gmail.com>
2023-09-03 20:10:37 -0400

    added From trait for Rows and open fn for Document

--- src/editor.rs
+++ src/editor.rs
@@ -3,6 +3,7 @@ use termion::event::Key;
 
 const VERSION: &str = env!("CARGO_PKG_VERSION");
 
+#[derive(Default)]
 pub struct Position {
     pub x: usize,
     pub y: usize,
@@ -33,14 +34,14 @@ impl Editor {
         Self {
             should_quit: false,
             terminal: Terminal::default().expect("Failed to initialize terminal"),
-            cursor_position: Position { x: 0, y: 0 },
-            document: Document::default(),
+            cursor_position: Position::default(),
+            document: Document::open(),
         }
     }
 
     fn refresh_screen(&self) -> Result<(), std::io::Error> {
         Terminal::cursor_hide();
-        Terminal::cursor_position(&Position { x: 0, y: 0 });
+        Terminal::cursor_position(&Position::default());
         if self.should_quit {
             Terminal::clear_screen();
             println!("Goodbye!")
